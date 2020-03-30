efae61e07176d9f8d07c0fdd9c60b6f256d3513d
seb <duchenne.sebastien@gmail.com>
2020-03-30 22:51:08 +0200

    react 30 mars 20

--- react/src/App.js
+++ react/src/App.js
@@ -1,67 +1,50 @@
 import React, { Component } from 'react';
-//import './App.css';
+import './App.css';
 
 class App extends Component {
-  constructor() {  
-    super();
-  }  
-
   render() {
     return (
       <div className="app">
-        <Menu />
-        <Event_list />
+        <AddEvent />
+        <EventList />
       </div>
     );
   }
 }
 
-class List extends React.Component {  
-  render() {  
+class List extends React.Component {
+  render() {
     return (
       <p>{this.props.data.event}</p>
-    );  
-  }  
-}  
-
-class Menu extends Component {
-  constructor() {  
-    super();
-    this.catg = {
-      data:   
-      [  
-        { "event":"c1" },
-        { "event":"c2" },
-        { "event":"c3" }  
-      ]  
-    }
+    );
   }
+}
 
+class AddEvent extends Component {
   render() {
     return (
       <div className="app">
-        <div id="menu">
-          {this.catg.data.map((item) => <List data = {item} />)}
-        </div>
         <div id="bt">
-          <button>add event</button>
+          <input type="text"/>
+          <button
+            onClick={() => alert("add")}
+          >Add event</button>
         </div>
       </div>
     );
   }
 }
 
-
-class Event_list extends Component {
-  constructor() {  
+class EventList extends Component {
+  constructor() {
     super();
     this.event = {
-      data:   
-      [  
+      data:
+      [
         { "event":"e1" },
         { "event":"e2" },
-        { "event":"e3" }   
-      ]  
+        { "event":"e3" }
+      ]
     }
   }
 
